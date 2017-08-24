b02b1749541152cdb6905573aee7642cc4f701f6
Beovulfo <31137373+Beovulfo@users.noreply.github.com>
2017-08-24 11:17:21 +0200

    New test
    Now we are getting closer. However, the output does not conserve the order when printing the top N airports...

--- ex5/topN.py
+++ ex5/topN.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import web
+import numpy as np
 import json
 import pandas as pd
 
@@ -11,9 +12,10 @@ class index:
     def GET(self,name):
         fnew = 'topN.h5';#File to read
         df = pd.read_hdf(fnew,mode='r')
-        output = df.head(name)
+        N = np.int(name)#Convert to integer
+	mydict = (df.head(N)).to_dict()
         web.header('Content-Type','application/json')
-        return json.dumps(output)
+        return json.dumps(mydict)
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
